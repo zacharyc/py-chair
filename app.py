1e24a4451c7e32aecd4444644e7f9acae46798d9
Zachary Cohen <zachary@zacharyc.com>
2021-10-04 08:03:46 -0400

    Building out a wheelchair class basics

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
 import sys
 
+menu="""
+Welcome to Py Chair -> A wheelchair simulation.
+Menu:
+1) Echo Print
+q) quit
+"""
+
+# Writing a class
+
+class WheelChair:
+	length = 0
+	width = 0
+
+	def __init__(self, length, width):
+		self.length = length
+		self.width = width
+
 def main():
 	while 1:
-		print("Enter a command, q to quit")
+		print(menu)
 		foo = str(input())
 
 		if (foo == "q"):
 			break;
+		elif (foo == "1"):
+			print("Enter a length in inches:")
+			l = str(input())
+			print("Enter a width in inches:")
+			w = str(input())
+			wc = WheelChair(l, w)
+
+			print(wc)
 		else:
 			print(foo)
 
